860c60e56e9139911e8546a3a34f0bf5ae4518e3
arrie <arriewang@gmail.com>
2015-10-12 19:20:18 +0800

    10/12

--- js/controllers.js
+++ js/controllers.js
@@ -3,31 +3,24 @@
  *完成后台城市数据的遍历，显示前端样式
  */
 var CityModule = angular.module('CityModule', []);
-CityModule.controller('citySyncCtrl', ['$scope', function($scope){
-	$scope.citys=[
-	{cityId:"1",cityName:"beijing"},
-	{cityId:"2",cityName:"tianjin"},
-	{cityId:"3",cityName:"shengyang"},
-	{cityId:"4",cityName:"changchun"},
-	{cityId:"5",cityName:"haerbin"},
-	{cityId:"6",cityName:"shenzhen"},
-	{cityId:"7",cityName:"shanghai"},
-	{cityId:"8",cityName:"guangzhou"}
-	];
-	$scope.remove = function (index) {
-            $scope.items.splice(index, 1);
-        }
-            // $http.get('../data/citys.json')
-            //     .success(function(data, status, headers, config) {
-            //             data = largeLoad.filter(function(item) {
-            //                 return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
-            //             });
-            //             $scope.setPagingData(data, page, pageSize);
-            // } else {
-            //     $http.get('../data/books' + $stateParams.bookType + '.json')
-            //         .success(function(largeLoad) {
-            //             $scope.setPagingData(largeLoad, page, pageSize);
-            //         });
-            //     }
-        
- }]);
+CityModule.controller('citySyncCtrl', function($scope, $http, $state, $stateParams){
+	// $scope.citys=[
+	// {cityId:"1",cityName:"beijing"},
+	// {cityId:"2",cityName:"tianjin"},
+	// {cityId:"3",cityName:"shengyang"},
+	// {cityId:"4",cityName:"changchun"},
+	// {cityId:"5",cityName:"haerbin"},
+	// {cityId:"6",cityName:"shenzhen"},
+	// {cityId:"7",cityName:"shanghai"},
+	// {cityId:"8",cityName:"guangzhou"}
+	// ];
+	// $scope.remove = function (index) {
+ //            $scope.items.splice(index, 1);
+ //        }
+            $http.get('../data/citys.json')
+                .success(function(data, status, headers, config) {
+                        $scope = JSON.parse(data);
+            }).error(function(data, status, headers, config) {
+                  console.log("error");
+            });
+ });
